3db23a66f126e3317fd3cbbe0d521cf451e1a939
momogeeg <mobalde1298@gmail.com>
2023-04-04 04:30:23 +0000

    commit_1

---
+++ srapping.py
@@ -0,0 +1,18 @@
+from requests import get
+from scrapy import Selector
+# Lien de la page à scraper
+url = "https://fr.wikipedia.org/wiki/Star_Wars"
+response = get(url)
+source = None # Le code source de la page 
+if response.status_code == 200 :
+    # Si la requete s'est bien passee
+    source = response.text
+
+if source :
+    # Si le code source existe
+    selector = Selector(text=source)
+    titles = selector.css("div.toc ul > li")
+    for title in titles:
+        level = title.css("span.tocnumber::text").extract_first()
+        name = title.css("span.toctext::text").extract_first()
+        print(level + " " + name)
